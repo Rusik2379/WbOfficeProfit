dc09cf7f71a24f14ee18ce6e1e504c432f625c30
Rusik2379 <rusikfrolik@gmail.com>
2025-07-25 13:06:23 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,9 @@ app = FastAPI()
 # Разрешаем CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+    "http://176.123.163.173",
+    "http://176.123.163.173:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
